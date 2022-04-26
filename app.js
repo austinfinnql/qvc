c2365bf4cdcc4a9f13f6cc4b4e83b5ca6d61f93c
Austin Finn <>
2022-04-26 16:21:09 +1000

    Wrap everything in a try/catch block

--- app.js
+++ app.js
@@ -5,40 +5,43 @@ const path = require('path');
 const unix = require('./executeUnixCommand')
 
 async function findNewestQuicktimeFile(){
-    const sourceDirectory = `/Users/austinfinn/Desktop/`
-    const finalList = []
+    try {
+        const sourceDirectory = `/Users/austinfinn/Desktdop/`
+        const finalList = []
 
-    const allFiles = await fs.readdirSync(sourceDirectory)
+        const allFiles = await fs.readdirSync(sourceDirectory)
 
-    const files = allFiles.filter((file) => {
-        if(file.endsWith('.mov')){
-            return file
-        }
-    })
-
-    for(const file of files){
-        const result = await fs.statSync(`${sourceDirectory}${file}`)
-        finalList.push({
-            fileName: file,
-            lastModifiedDate: result.mtime
+        const files = allFiles.filter((file) => {
+            if(file.endsWith('.mov')){
+                return file
+            }
         })
-    };
 
-    finalList.sort(sortByLastModifiedDate)
-    finalList.reverse()
+        for(const file of files){
+            const result = await fs.statSync(`${sourceDirectory}${file}`)
+            finalList.push({
+                fileName: file,
+                lastModifiedDate: result.mtime
+            })
+        };
 
-    const fileName = path.parse(finalList[0].fileName).name
+        finalList.sort(sortByLastModifiedDate)
+        finalList.reverse()
 
-    const command = `ffmpeg -i '${sourceDirectory}${fileName}.mov' '${sourceDirectory}${fileName}.mp4' -y`
-    await unix.exec(command)
+        const fileName = path.parse(finalList[0].fileName).name
 
-    console.info(` Successfully converted '${finalList[0].fileName}' to .mp4 format`)
-    console.info(`\n Video location: ${sourceDirectory}\n`)
+        const command = `ffmpeg -i '${sourceDirectory}${fileName}.mov' '${sourceDirectory}${fileName}.mp4' -y`
+        await unix.exec(command)
+
+        console.info(` Successfully converted '${finalList[0].fileName}' to .mp4 format`)
+        console.info(`\n Video location: ${sourceDirectory}\n`)
+    } catch (error) {
+        console.log('Error occured ... ', error)
+    }
 }
 
 function sortByLastModifiedDate(a, b) {
     return a.lastModifiedDate - b.lastModifiedDate;
 }
-
 
 findNewestQuicktimeFile()
