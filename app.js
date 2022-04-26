fa366a576bf297787b858065921fcb6b5f3b078d
Austin Finn <>
2022-04-26 19:03:51 +1000

    Change the application to proactively convert .mov files to .mp4

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const fs = require('fs')
 const path = require('path');
 const unix = require('./executeUnixCommand')
+const chokidar = require('chokidar');
 
 async function findNewestQuicktimeFile(){
     const sourceDirectory = `/Users/austinfinn/Desktop/`
@@ -58,4 +59,35 @@ function sortByLastModifiedDate(a, b) {
     return a.lastModifiedDate - b.lastModifiedDate;
 }
 
-findNewestQuicktimeFile()
+async function listenForMOVfiles(){
+    const sourceDirectory = `/Users/austinfinn/Desktop/`
+
+    try {
+        const watcher = chokidar.watch(sourceDirectory, {
+            ignored: /(^|[\/\\])\../, // ignore dotfiles
+            ignoreInitial: true, // ignore file already in the directory
+            persistent: true
+        })
+
+        watcher.on('add', async (absoluteFilePath) => {
+
+            const fileNameWithExtension = path.parse(absoluteFilePath).base
+            
+            if(fileNameWithExtension.endsWith('.mov')){
+                const fileName = path.parse(absoluteFilePath).name
+
+                console.info()
+                console.info(` New .mov file found`)
+                console.info(` Converting to .mp4; Please wait...`)
+                console.info()
+
+                const command = `ffmpeg -i '${sourceDirectory}${fileName}.mov' '${sourceDirectory}${fileName}.mp4' -y`
+                await unix.exec(command)
+            }
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+listenForMOVfiles()
