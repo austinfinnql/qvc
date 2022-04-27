d436d07cc8c18d74cea2586a84a7ffd9f14a9932
Austin Finn <>
2022-04-27 11:45:52 +1000

    Create separate function to convert the video. Also all helper functions and modules to a separate directory

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 #!/usr/bin/env node
 
-const path = require('path');
-const unix = require('./executeUnixCommand')
 const chokidar = require('chokidar');
+const helper = require('./helpers/helper')
 
 async function listenForMOVfiles(){
     try {
         if(!process.argv[2]) throw 'MISSING_WATCH_DIRECTORY'
 
-        watchDirectory = validateWatchDirectoryValue(process.argv[2])
+        watchDirectory = helper.validateWatchDirectoryValue(process.argv[2])
 
         console.info(`\n Listening for new .mov files in '${watchDirectory}'\n`)
 
@@ -18,34 +17,14 @@ async function listenForMOVfiles(){
             persistent: true
         })
 
-        watcher.on('add', (absoluteFilePath) => {
+        watcher.on('add', helper.convertVideoToMP4)
 
-            const fileNameWithExtension = path.parse(absoluteFilePath).base
-            
-            if(fileNameWithExtension.endsWith('.mov')){
-                const fileName = path.parse(absoluteFilePath).name
-
-                console.info()
-                console.info(` New .mov file found`)
-                console.info(` Converting to .mp4; Please wait...`)
-                console.info()
-
-                const command = `ffmpeg -i '${watchDirectory}${fileName}.mov' '${watchDirectory}${fileName}.mp4' -y`
-                unix.exec(command)
-            }
-        })
     } catch (error) {
         if(error == 'MISSING_WATCH_DIRECTORY'){
             console.info('\n Please include the path to the directory where Quicktime videos are saved e.g. ~/Desktop/\n')
         } else {
             console.log(error)
         }
-    }
-}
-
-function validateWatchDirectoryValue(watchDirectory) {
-    if(!watchDirectory.endsWith('/')){
-        return watchDirectory + '/'
     }
 }
 
---
+++ helpers/helper.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const unix = require('./executeUnixCommand')
+
+function convertVideoToMP4(absoluteFilePath) {
+    const watchDirectory = validateWatchDirectoryValue(process.argv[2])
+    const fileNameWithExtension = path.parse(absoluteFilePath).base
+
+    if(fileNameWithExtension.endsWith('.mov')){
+        const fileName = path.parse(absoluteFilePath).name
+
+        console.info()
+        console.info(` New .mov file found`)
+        console.info(` Converting to .mp4; Please wait...`)
+        console.info()
+
+        const command = `ffmpeg -i '${watchDirectory}${fileName}.mov' '${watchDirectory}${fileName}.mp4' -y`
+        unix.exec(command)
+    }
+}
+
+function validateWatchDirectoryValue(watchDirectory) {
+    if(!watchDirectory.endsWith('/')){
+        return watchDirectory + '/'
+    }
+}
+
+module.exports = {
+    validateWatchDirectoryValue,
+    convertVideoToMP4
+}
